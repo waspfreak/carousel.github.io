85ae1ea9b7794dc5b3cbda2515177cb483c0d7d7
zo17 <juliana.leon@global.tesco.org>
2016-06-22 13:27:32 +0100

    update css for mobile img use in component

--- curso/server.js
+++ curso/server.js
@@ -6,6 +6,6 @@ var app = express();
 
 app.use(express.static('public'));
 
-app.listen(3000, function(){
-  console.log('Express server is up on port 3000');
+app.listen(8000, function(){
+  console.log('Express server is up on port 8000');
 });
